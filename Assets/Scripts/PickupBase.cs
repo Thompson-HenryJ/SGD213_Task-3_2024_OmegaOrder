0f161a001e1576d89078035f1f2a6d40f50dc435
EdAdams12 <E_A055@student.usc.edu.au>
2024-05-18 14:50:58 +1000

    PickupBase Script Changes
    Changed some of the summaries within the PickupBase script. Trying to make it easier to understand the process of what is needed. Also added in some code to the main abstract class, just a first mockup part of the script

--- Assets/Scripts/PickupBase.cs
+++ Assets/Scripts/PickupBase.cs
@@ -2,45 +2,91 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Pickups
+public abstract class PickupBase
 {
     public float respawnDelay = 10;
-    public object respawnSFX;
-    public object pickupSFX;
-    bool respawn = true;
+    public AudioClip respawnSFX;
+    public AudioClip pickupSFX;
+    protected bool respawn = true;
 
-    /// - Check if interaction is with playercharacter
-    /// - If false do nothing, if true and health pickup increase playercharacter health by X
-    /// 
-    /// - if ammo pickup increase the maxammo or current ammo by X
-    /// - Finally if it's the weapon pickup and is the same weapon do nothing but if the weapon is different
-    /// 
-    /// - Remove current weapon on player character, add weapon on pickup to character
-    /// - For all pickups after interection with playercharacter destroy the pickup object + play pickupSFX
-    /// - Start respawntimer (respawn delay) and once respawned play respawnSFX
+    /// - Check if it's the playerCharacter thats interacting with the pickup
+    /// - False = do nothing
+    /// - True = follow respective pickup below
+
+    protected virtual void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayPickupSound();
+            ApplyEffect(other.gameObject);
+            Destroy(gameObject);
+            StartRespawnTimer();
+        }
+    }
+
+    protected abstract void ApplyEffect(GameObject player);
+
+    protected virtual void PlayerPickupSound()
+    {
+        if (pickupSFX != null)
+        {
+            AudioSource.PlayClipAtPoint(pickupSFX, transform.position);
+        }
+    }
+
+    protected virtual void StartRespawnTimer()
+    {
+        respawn = false;
+        Invoke(nameof(Respawn), respawnDelay);
+    }
+
+    protected virtual void Respawn()
+    {
+        respawn = true;
+        if(respawnSFX != null)
+        {
+            AudioSource.PlayClipAtPoint(respawnSFX, transform.position);
+        }
+    }
 }
 
-class healthPickup : Pickups
+class healthPickup : PickupBase
 {
     public float healAmount; 
 
-    /// If health pickup...increase playercharacters health by X ammount
+    /// Play pickupSFX
+    /// Destroy healthPickup
+    /// Increase playerCharacters health by X ammount (Max Health being the limit)
+    
+    /// Start respawn timer and when it's equal to '0' respawn health pickup
+    /// Play respawnSFX when respawn timer equals 0
  
 }
 
-class ammoPickup : Pickups
+class ammoPickup : PickupBase
 {
     public int ammoAmount; 
 
-    /// Ammo pickup, increase maxAmmo ammount by X ammount
+    /// Play pickupSFX
+    /// Destroy ammoPickup
+    /// Increase maxAmmo value by X ammount
+    /// Start respawn timer and when it's equal to '0' respawn ammo pickup
+    /// Play respawnSFX when respawn timer equals 0
+
 }
 
-class weaponPickup : Pickups
+class weaponPickup : PickupBase
 {
     public object weapon;
     bool collectionEnabled = true;
 
-    /// Is interaction with Playercharacter 
-    /// Remove or destroy current weapon
-    /// Add new weapon to playercharacter
+    /// If 'weaponPickup == currentWeapon' then 'collectionEnabled = False' then do nothing and return
+    
+    /// If 'weaponPickup != currentWeapon' then 'collectionEnabled = True' then do the following below
+    /// playerCharacter's current weapon = Destroyed
+    /// playerCharacter picks up the new weapon
+    /// Start respawn timer 
+    /// timer = 0 then spawn new weapon
+    /// Play respawnSFX when respawn timer equals 0
+
 }
