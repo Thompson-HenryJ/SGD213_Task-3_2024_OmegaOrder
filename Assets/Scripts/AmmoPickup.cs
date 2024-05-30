a80359578c683b8cce2981923ce89d68360b13e3
EdAdams12 <E_A055@student.usc.edu.au>
2024-05-30 13:48:08 +1000

    Minor Comments Added
    Small comments added, not a huge commit. Just an overall look at what we're working with

--- Assets/Scripts/AmmoPickup.cs
+++ Assets/Scripts/AmmoPickup.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 class AmmoPickup : PickupBase
 {
-    public int ammoAmount;
+    [SerializeField] public int ammoAmount;
 
     /// Play pickupSFX
     /// Destroy ammoPickup
     /// Increase maxAmmo value by X ammount
     /// Start respawn timer and when it's equal to '0' respawn ammo pickup
     /// Play respawnSFX when respawn timer equals 0
+    
     protected override void ApplyEffect(GameObject player)
     {
-
+        // Weapon1.AddAmmo(AmmoAmount) 
     }
 }
--- Assets/Scripts/WeaponPickup.cs
+++ Assets/Scripts/WeaponPickup.cs
@@ -15,8 +15,15 @@ class WeaponPickup : PickupBase
     /// Start respawn timer 
     /// timer = 0 then spawn new weapon
     /// Play respawnSFX when respawn timer equals 0
+
     protected override void ApplyEffect(GameObject player)
     {
+        // Check if player holds weapon1 or weapon2
 
+        // If weapon1, then do the following
+        // Check if weapon1 == weaponPickup, if true do nothing and return
+        // If false then then change set new weapon as weapon1
+
+        // If weapon2, do the same but with weapon2
     }
 }
